24d607e25110c412e9fa755b4b0b2d10a63ff590
hseguraq <91026821+hseguraq@users.noreply.github.com>
2024-12-20 17:37:00 -0600

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'Breast Cancer Risk Index',
-    description: "Part of a proposal for a monitoring index that would provide timely georeferenced information about breast cancer risks in mexican women",
+    description: "Part of a proposal for a monitoring index that would provide timely georeferenced information about breast cancer risks in mexican women.",
     image: '/images/bcimage2.jpg',
     tags: ['Javascript', 'Streamlit', 'APIs'],
     visit: 'https://monitorbcslpmun.streamlit.app',
@@ -9,7 +9,7 @@ export const projects = [
   },
   {
     title: 'Continous Index Monitor and Image Classification',
-    description: "This is merely a front-end  suggestion of how a real-time monitoring of the index would look like. At the bottom left is a link to the site where radiologists can upload images for a CNN to classify it with a certain degree of risk.",
+    description: "This is merely a front-end suggestion of how a real-time monitoring of the index would look like. At the bottom left is a link to the site where radiologists can upload images for a CNN to assign it a BI-RADS code.",
     image: '/images/bcsystem.jpg',
     tags: ['HTML', 'CSS', 'Javascript'],
     visit: 'https://hseguraq.github.io/systemBC/',
@@ -17,7 +17,7 @@ export const projects = [
   },
   {
     title: 'Weather App',
-    description: "A weather app that is responsive and connected to the Open Weather Map API. User can enter any city name and basic weather conditions will be provided",
+    description: "A weather app that is responsive and connected to the Open Weather Map API. User can enter any city name and basic weather conditions will be provided.",
     image: '/images/weathering.jpg',
     tags: ['React', 'Javascript', 'APIs'],
     visit: 'https://weathering-w-u.netlify.app',
@@ -25,7 +25,7 @@ export const projects = [
   },
   {
     title: 'High Tech Landing Page',
-    description: "A landing page for ReziVR, a fictional company dedicated to provide software tools for VR developers. Contains all sections a user would expect from such an enterprise",
+    description: "A landing page for ReziVR, a fictional company dedicated to provide software tools for VR developers. Contains all sections a user would expect from such an enterprise.",
     image: '/images/rezivr.jpg',
     tags: ['React', 'Vite', 'Tailwind', 'Vercel'],
     visit: 'https://rezi-vr-hsq.vercel.app',
@@ -35,12 +35,4 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2016, text: 'Started my journey as a developer.', },
-  { year: 2017, text: 'Worked intensively on ROMs & Kernels.', },
-  { year: 2018, text: 'Learned Android app development.', },
-  { year: 2019, text: 'Finished Diploma in Computer Engineering.', },
-  { year: 2020, text: 'Aquired new skills while trying not to catch COVID-19', },
-  { year: 2021, text: 'Survived COVID-19 and got my first Job at Primebook.', },
-  { year: 2022, text: 'Completed B.Tech in Information Technology.', },
-  { year: 2023, text: 'Year under progress....', },
 ];
